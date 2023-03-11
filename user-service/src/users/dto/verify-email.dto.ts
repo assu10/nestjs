2931420071deab31fd20d4d1fd817e28a39a126a
assu10 <juhyun10.lee@gmail.com>
2023-03-11 17:40:24 +0900

    no message

---
+++ user-service/src/users/dto/verify-email.dto.ts
@@ -0,0 +1,4 @@
+export class VerifyEmailDto {
+  // 이메일 인증 시 URL 에 포함되어 오는 쿼리 매개변수
+  signupVerifyToken: string;
+}
---
+++ user-service/src/users/users.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './UserInfo';
+
+@Controller('users')
+export class UsersController {
+  // 회원 가입
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log('createUser dto: ', dto);
+  }
+
+  // 이메일 인증
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log('verifyEmail dto: ', dto);
+    return;
+  }
+
+  // 로그인
+  @Post('login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log('login dto: ', dto);
+    return;
+  }
+
+  // 회원 정보 조회
+  @Get(':id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log('getUserInfo userId: ', userId);
+    return;
+  }
+}
