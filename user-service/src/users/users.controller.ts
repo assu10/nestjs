4350b7c1a9d00db95c02117ff4bc08d9af931b5a
assu10 <juhyun10.lee@gmail.com>
2023-03-13 15:19:33 +0900

    ch04. NestJS - Provider: user service

--- user-service/src/users/users.controller.ts
+++ user-service/src/users/users.controller.ts
@@ -3,33 +3,40 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserInfo } from './UserInfo';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  // UsersService 를 컨트롤러에 주입
+  constructor(private readonly usersService: UsersService) {}
   // 회원 가입
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    const { name, email, password } = dto;
     console.log('createUser dto: ', dto);
+    await this.usersService.createUser(name, email, password);
   }
 
   // 이메일 인증
   @Post('/email-verify')
   async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
     console.log('verifyEmail dto: ', dto);
-    return;
+    return await this.usersService.verifyEmail(signupVerifyToken);
   }
 
   // 로그인
   @Post('login')
   async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
     console.log('login dto: ', dto);
-    return;
+    return await this.usersService.login(email, password);
   }
 
-  // 회원 정보 조회
+  // 유저 정보 조회
   @Get(':id')
   async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
     console.log('getUserInfo userId: ', userId);
-    return;
+    return await this.usersService.getUserInfo(userId);
   }
 }
