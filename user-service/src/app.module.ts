2738819adcfe607c824a674d830202d9ea6bda1d
assu10 <juhyun10.lee@gmail.com>
2023-03-24 13:44:52 +0900

    ch06. NestJS - Module 설계: user-service

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig], // ConfigFactory 지정
+      isGlobal: true, // 전역으로 등록해서 어느 모듈에서나 사용 가능
+      validationSchema, // 환경 변수 값에 대해 유효성 검사 수행
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- user-service/src/email/email.service.ts
+++ user-service/src/email/email.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import Mail from 'nodemailer/lib/mailer';
 import * as nodemailer from 'nodemailer';
+import emailConfig from '../config/emailConfig';
+import { ConfigType } from '@nestjs/config';
 
 interface EmailOptions {
   to: string;
@@ -11,19 +13,32 @@ interface EmailOptions {
 export class EmailService {
   private transporter: Mail;
 
-  constructor() {
+  // constructor() {
+  //   this.transporter = nodemailer.createTransport({
+  //     service: 'Gmail',
+  //     auth: {
+  //       user: 'test@gmail.com',
+  //       pass: 'aaaaa',
+  //     },
+  //   });
+  // }
+
+  constructor(
+    // 주입받을 때 @Inject 데커레이터의 토큰을 앞에서 만든 ConfigFactory 의 KEY 인 `email` 문자열로 넣어준다.
+    @Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>,
+  ) {
     this.transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: config.service, // env 파일에 있는 값들
       auth: {
-        user: 'test@gmail.com',
-        pass: 'aaaaa',
+        user: config.auth.user,
+        pass: config.auth.pass,
       },
     });
   }
 
   // 가입 인증 메일 발송
   async sendMemberJoinVerification(email: string, signupVerifyToken: string) {
-    const baseUrl = 'http://localhost:3000';
+    const baseUrl = this.config.baseUrl;
     const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
     const mailOptions: EmailOptions = {
       to: email,
